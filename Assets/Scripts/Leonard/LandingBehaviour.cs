f70cc85093c4e8e0259e34a7063d6bd9c9464627
Léonard <epoc4oftime@gmail.com>
2021-04-27 10:14:22 +0200

    Landing Behaviour

--- Assets/Scripts/Leonard/LandingBehaviour.cs
+++ Assets/Scripts/Leonard/LandingBehaviour.cs
@@ -1,8 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class LandingBehaviour : MonoBehaviour
 {
-    
+    private bool isOverCity;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y) && isOverCity) LandingSequence();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<CityBehaviour>()) isOverCity = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {        
+        if (other.GetComponent<CityBehaviour>()) isOverCity = false;
+    }
+
+    private void LandingSequence()
+    {
+        Debug.Log("Landed in City");
+    }
 }
