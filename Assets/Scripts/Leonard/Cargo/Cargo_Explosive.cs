dea627f245bb6f06f8df71e01407e7d828b2181b
Léonard <epoc4oftime@gmail.com>
2021-04-28 11:37:13 +0200

    Implemented Cargo Hold Logic

--- Assets/Scripts/Leonard/Cargo/Cargo_Explosive.cs
+++ Assets/Scripts/Leonard/Cargo/Cargo_Explosive.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cargo_Explosives : AbstractCargo
+public class Cargo_Explosive : AbstractCargo
 {
     public override void ApplyEffect() => Debug.Log("Explosives");
 }
--- Assets/Scripts/Leonard/Manager/CargoManager.cs
+++ Assets/Scripts/Leonard/Manager/CargoManager.cs
@@ -1,31 +1,120 @@
-using System;
 using Sirenix.OdinInspector;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class CargoManager : MonoBehaviour
 {
-    [ShowInInspector] Dictionary<AbstractCargo, int> cargoHold = new Dictionary<AbstractCargo, int>();
+    [ShowInInspector] Dictionary<ItemType, int> cargoTypes = new Dictionary<ItemType, int>();
+    private List<AbstractCargo> cargoHold = new List<AbstractCargo>();
 
-    private void Update() => ApplyAllEffects();
+    void Update()
+    {
+        foreach (var cargoItem in cargoHold)
+            cargoItem.ApplyEffect();
+
+        if (Input.GetKeyDown(KeyCode.B)) AddCargo(ItemType.Bananas);
+        if (Input.GetKeyDown(KeyCode.C)) AddCargo(ItemType.Crocodile);
+        if (Input.GetKeyDown(KeyCode.D)) RemoveCargo(ItemType.Crocodile);
+        if (Input.GetKeyDown(KeyCode.E)) RemoveCargo(ItemType.Crocodile);
+    }
 
     // called when the player buys cargo of a certain type
-    public void AddCargo(AbstractCargo cargoType)
+    public void AddCargo(ItemType cargoType)
     {
-        if (cargoHold.ContainsKey(cargoType)) cargoHold[cargoType]++; // increment the number of items in the hold
-        else cargoHold.Add(cargoType, 1); // create a new item
+        if (!cargoTypes.ContainsKey(cargoType))
+        {
+            cargoTypes.Add(cargoType, 0); // create a new item with 
+
+            switch (cargoType)
+            {
+                case ItemType.Bananas:
+                    cargoHold.Add(gameObject.AddComponent<Cargo_Banana>());
+                    break;
+                case ItemType.Bottles:
+                    cargoHold.Add(gameObject.AddComponent<Cargo_Bottles>());
+                    break;
+                case ItemType.Crocodile:
+                    cargoHold.Add(gameObject.AddComponent<Cargo_Crocodile>());
+                    break;
+                case ItemType.Eggs:
+                    cargoHold.Add(gameObject.AddComponent<Cargo_Eggs>());
+                    break;
+                case ItemType.Explosive:
+                    cargoHold.Add(gameObject.AddComponent<Cargo_Explosive>());
+                    break;
+                case ItemType.Grease:
+                    cargoHold.Add(gameObject.AddComponent<Cargo_Grease>());
+                    break;
+                case ItemType.Magnets:
+                    cargoHold.Add(gameObject.AddComponent<Cargo_Magnets>());
+                    break;
+                case ItemType.Ore:
+                    cargoHold.Add(gameObject.AddComponent<Cargo_Ore>());
+                    break;
+                case ItemType.Paintings:
+                    cargoHold.Add(gameObject.AddComponent<Cargo_Painting>());
+                    break;
+                default:
+                    Debug.Log("Couldn't add that type of cargo");
+                    break;
+            }
+        }
+
+        cargoTypes[cargoType]++; // increment the number of items in the hold
     }
 
     // called when the player sells cargo of a certain type
-    public void RemoveCargo(AbstractCargo cargoType)
+    public void RemoveCargo(ItemType cargoType)
     {
-        --cargoHold[cargoType]; // decrement the number of items in the hold
-        if (cargoHold[cargoType] == 0) cargoHold.Remove(cargoType); // remove items if it was the last one
-    }
-    
-    void ApplyAllEffects()
-    {
-        foreach (KeyValuePair<AbstractCargo, int> cargoItem in cargoHold)
-            cargoItem.Key.ApplyEffect();
+        --cargoTypes[cargoType]; // decrement the number of items in the hold
+
+        if (cargoTypes[cargoType] == 0)
+        {
+            cargoTypes.Remove(cargoType); // remove items if it was the last one
+
+            switch (cargoType)
+            {
+                case ItemType.Bananas:
+                    cargoHold.Remove(gameObject.GetComponent<Cargo_Banana>());
+                    Destroy(gameObject.GetComponent<Cargo_Banana>());
+                    break;
+                case ItemType.Bottles:
+                    cargoHold.Remove(gameObject.GetComponent<Cargo_Bottles>());
+                    Destroy(gameObject.GetComponent<Cargo_Bottles>());
+                    break;
+                case ItemType.Crocodile:
+                    cargoHold.Remove(gameObject.GetComponent<Cargo_Crocodile>());
+                    Destroy(gameObject.GetComponent<Cargo_Crocodile>());
+                    break;
+                case ItemType.Eggs:
+                    cargoHold.Remove(gameObject.GetComponent<Cargo_Eggs>());
+                    Destroy(gameObject.GetComponent<Cargo_Eggs>());
+                    break;
+                case ItemType.Explosive:
+                    cargoHold.Remove(gameObject.GetComponent<Cargo_Explosive>());
+                    Destroy(gameObject.GetComponent<Cargo_Explosive>());
+                    break;
+                case ItemType.Grease:
+                    cargoHold.Remove(gameObject.GetComponent<Cargo_Grease>());
+                    Destroy(gameObject.GetComponent<Cargo_Grease>());
+                    break;
+                case ItemType.Magnets:
+                    cargoHold.Remove(gameObject.GetComponent<Cargo_Magnets>());
+                    Destroy(gameObject.GetComponent<Cargo_Magnets>());
+                    break;
+                case ItemType.Ore:
+                    cargoHold.Remove(gameObject.GetComponent<Cargo_Ore>());
+                    Destroy(gameObject.GetComponent<Cargo_Ore>());
+                    break;
+                case ItemType.Paintings:
+                    cargoHold.Remove(gameObject.GetComponent<Cargo_Painting>());
+                    Destroy(gameObject.GetComponent<Cargo_Painting>());
+                    break;
+                default:
+                    Debug.Log("Couldn't destroy that type of cargo");
+                    break;
+            }
+        }
     }
 }
