e2fef78cbf565118208bcf2f67c43534e6ec9bf0
Louis Nillus <louis.nillus@gmail.com>
2021-04-29 11:07:38 +0200

    Keyboard Navigation done + Clicks Catch

---
+++ Assets/Scripts/LON_Scripts/CatchMouseClicks.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CatchMouseClicks : MonoBehaviour
+{
+
+    GameObject lastSelected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(EventSystem.current.currentSelectedGameObject != null)
+        lastSelected = EventSystem.current.currentSelectedGameObject;
+
+        Debug.Log(lastSelected);
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+        {
+            CatchClicks();
+        }
+
+    }
+
+
+    public void CatchClicks()
+    {
+
+        EventSystem.current.SetSelectedGameObject(lastSelected);
+
+    }
+}
