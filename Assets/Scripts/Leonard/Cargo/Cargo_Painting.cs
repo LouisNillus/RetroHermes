8676f1a36f2e39b6c4d0d7db81f65e91ab5be09f
Léonard <epoc4oftime@gmail.com>
2021-04-29 11:14:38 +0200

    Cargo_Painting Damage Behaviour Implemented

--- Assets/Scripts/Leonard/Cargo/Cargo_Painting.cs
+++ Assets/Scripts/Leonard/Cargo/Cargo_Painting.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class Cargo_Painting : AbstractCargo
 {
     private float startDamaging = 5f;
-    private float elapsedtime = 0f;
+    private float startDamagingTime = 0f;
     
+    private float damageFrequency = 1f;
+    private float elapsedtime = 1f;
+
+    private float damagePercentage = 0.05f; 
+   
     public override void ApplyEffect()
     {
         if (PlaneManager.instance.isInCloud)
         {
-            elapsedtime += Time.deltaTime;
-
-            if (elapsedtime >= startDamaging)
+            // if in cloud for more than 5 seconds
+            if (startDamagingTime >= startDamaging)
             {
-                elapsedtime = 0;
-                
+                // every 1 second
+                if (elapsedtime >= damageFrequency)
+                {
+                    elapsedtime = 0;
+                    SpecifyDamage(damagePercentage, DamageType.Cloud);
+                }
+
+                elapsedtime += Time.deltaTime;
             }
+            else startDamagingTime += Time.deltaTime;
         }
+        
+        else startDamagingTime = 0;
     }
 }
